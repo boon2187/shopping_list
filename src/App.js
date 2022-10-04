cf51a7caf5d17433cf52777842543c3e6aa56ea7
boon2187 <fumiya.mai.ito@gmail.com>
2022-10-04 15:55:46 +0900

    アイテム追加の関数（ひな形）

--- src/App.js
+++ src/App.js
@@ -6,10 +6,14 @@ function App() {
   // 買い物アイテムを格納するステート
   const [items, setItems] = useState([]);
 
+  const addShopItem = () => {
+    
+  }
+
   return (
     <div className="App">
       <input type="text" placeholder="追加アイテム" ></input>
-      <button>アイテム追加</button>
+      <button onClick={addShopItem}>アイテム追加</button>
       <Item />
     </div>
   );
