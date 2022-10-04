fae12c434693e8a12111821935413126cfee4afe
boon2187 <fumiya.mai.ito@gmail.com>
2022-10-04 16:39:55 +0900

    とりあえずひな形まで

--- src/App.js
+++ src/App.js
@@ -3,18 +3,19 @@ import './App.css'
 import Item from './compontents/Item';
 
 function App() {
+  // Inputに渡された値を収納するステート
+  const [value, setValue] = useState("");
   // 買い物アイテムを格納するステート
-  const [items, setItems] = useState([]);
+  const [shopItmes, setShopItems] = useState([]);
 
   const addShopItem = () => {
-    
+ 
   }
 
   return (
     <div className="App">
-      <input type="text" placeholder="追加アイテム" ></input>
+      <input type="text" placeholder="追加アイテム" value={value} onChange={(e) => setValue(e.target.value)}></input>
       <button onClick={addShopItem}>アイテム追加</button>
-      <Item />
     </div>
   );
 }
