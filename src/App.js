afc1382cb33028bd1c08bf60e7727b2b9f496ff1
boon2187 <fumiya.mai.ito@gmail.com>
2022-10-04 14:02:32 +0900

    メインページの構成をざっくりと

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
-import './App.css';
+import { useState } from 'react';
+import './App.css'
+import Item from './compontents/Item';
 
 function App() {
+  // 買い物アイテムを格納するステート
+  const [items, setItems] = useState([]);
+
   return (
     <div className="App">
-      
+      <input type="text" placeholder="追加アイテム" ></input>
+      <button>アイテム追加</button>
+      <Item />
     </div>
   );
 }
