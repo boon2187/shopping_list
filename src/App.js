1ee12aa04fd7760c4a8f9fd0336ecc4d1666443f
boon2187 <fumiya.mai.ito@gmail.com>
2022-10-06 14:11:27 +0900

    アイテムを格納するステートをとりあえず作る

--- src/App.js
+++ src/App.js
@@ -5,8 +5,13 @@ import Item from './compontents/Item';
 function App() {
   // Inputに渡された値を収納するステート
   // const [value, setValue] = useState("");
-  // 買い物アイテムを格納するステート
-  // const [shopItmes, setShopItems] = useState([]);
+  買い物アイテムを格納するステート
+  const [shopItmes, setShopItems] = useState([
+    "item 1",
+    "item 2",
+    "item 3",
+    "item 4"
+  ]);
 
   // const addShopItem = () => {
  
