828318e7f6e5f81feb399302873960955a720a15
boon2187 <fumiya.mai.ito@gmail.com>
2022-10-06 14:07:00 +0900

    実装前の準備

--- src/App.js
+++ src/App.js
@@ -4,18 +4,42 @@ import Item from './compontents/Item';
 
 function App() {
   // Inputに渡された値を収納するステート
-  const [value, setValue] = useState("");
+  // const [value, setValue] = useState("");
   // 買い物アイテムを格納するステート
-  const [shopItmes, setShopItems] = useState([]);
+  // const [shopItmes, setShopItems] = useState([]);
 
-  const addShopItem = () => {
+  // const addShopItem = () => {
  
-  }
+  // }
 
   return (
     <div className="App">
-      <input type="text" placeholder="追加アイテム" value={value} onChange={(e) => setValue(e.target.value)}></input>
-      <button onClick={addShopItem}>アイテム追加</button>
+      <div className="container" >
+        <div className="shopping-list">
+          <div className="input-area">
+            <input type="text" placeholder="追加アイテム" ></input>
+            <button>+</button>
+          </div>
+          <hr />
+          <div className="shopping-items">
+            <ul>
+              <div className="item-row">
+                <li>item 1</li>
+              </div>
+              <div className="item-row">
+                <li>item 1</li>
+              </div>
+              <div className="item-row">
+                <li>item 2</li>
+              </div>
+              <div className="item-row">
+                <li>item 3</li>
+              </div>
+            </ul>
+            <p>total:3</p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
