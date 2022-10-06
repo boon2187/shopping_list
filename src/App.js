ef257b0f507f84ef14c43b8abdcdbb48ac20852d
boon2187 <fumiya.mai.ito@gmail.com>
2022-10-06 14:14:58 +0900

    調子にのってコメントもコメントアウトしてしまった…ｗ

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Item from './compontents/Item';
 function App() {
   // Inputに渡された値を収納するステート
   // const [value, setValue] = useState("");
-  買い物アイテムを格納するステート
+  // 買い物アイテムを格納するステート
   const [shopItmes, setShopItems] = useState([
     "item 1",
     "item 2",
