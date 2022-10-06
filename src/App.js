916325ef41173508d6a50fd926c8ef1a51194be0
boon2187 <fumiya.mai.ito@gmail.com>
2022-10-06 16:16:35 +0900

    shopItemsを名前でなく、オブジェクトの配列に変更

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import { useState } from 'react';
 import './App.css'
-import Item from './compontents/Item';
+// import Item from './compontents/Item';
 
 function App() {
   // Inputに渡された値を収納するステート
   // const [value, setValue] = useState("");
   // 買い物アイテムを格納するステート
   const [shopItems, setShopItems] = useState([
-    "item 1",
-    "item 2",
-    "item 3",
-    "item 4"
+    {itemName:"item 1", quantity: 2},
+    {itemName:"item 2", quantity: 1},
+    {itemName:"item 3", quantity: 4},
+    {itemName:"item 4", quantity: 3},
   ]);
 
   // const addShopItem = () => {
@@ -28,10 +28,10 @@ function App() {
           <hr />
           <div className="shopping-items">
             <ul>
-              {shopItems.map((item) => {
+              {shopItems.map((item, i) => {
                 return(
-                <div key={item} className="item-row">
-                  <li>{item}</li>
+                <div key={i} className="item-row">
+                  <li>{item.itemName} {item.quantity}</li>
                 </div>
                 )
               })}
