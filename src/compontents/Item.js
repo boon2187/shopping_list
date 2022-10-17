93034d465be6e7ccddfd2cc21996d5d8c27fd441
boon2187 <fumiya.mai.ito@gmail.com>
2022-10-17 16:35:08 +0900

    Itemコンポーネントに削除ボタンを追加

--- src/compontents/Item.js
+++ src/compontents/Item.js
@@ -2,13 +2,15 @@ import React from 'react'
 import "./item.css"
 
 const Item = (props) => {
-  const {itemName, quantity} = props;
+  const {itemName, quantity, deleteShopItem, index} = props;
+
+
 
   return (
     <div className='item-component-item'>
       <p>{itemName}</p>
       <p>{quantity}</p>
-      <button>削除</button>
+      <button onClick={() => deleteShopItem(index)}>削除</button>
     </div>
   )
 }
