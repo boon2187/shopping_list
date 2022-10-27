1f220e03fa8b2b020ed787e9ada624afc13e753c
boon2187 <fumiya.mai.ito@gmail.com>
2022-10-27 14:09:50 +0900

    プラスボタンとマイナスボタンを追加。ひな形の関数も追加。

--- src/compontents/Item.js
+++ src/compontents/Item.js
@@ -4,12 +4,23 @@ import "./item.css"
 const Item = (props) => {
   const {itemName, quantity, deleteShopItem, index} = props;
 
+  // +ボタンで増加
+  const increaseQuantity = () => {
+    console.log("+が押されたよ")
+  }
+
+  //　-ボタンで減少
+  const decreaseQuantity = () => {
+    console.log("-が押されたよ")
+  }
 
 
   return (
     <div className='item-component-item'>
       <p>{itemName}</p>
+      <button onClick={() => decreaseQuantity()}>-</button>
       <p>{quantity}</p>
+      <button onClick={() => increaseQuantity()}>+</button>
       <button onClick={() => deleteShopItem(index)}>削除</button>
     </div>
   )
