501d166eb66f7b461db00b9466a131b02f878600
boon2187 <fumiya.mai.ito@gmail.com>
2022-10-28 10:27:39 +0900

    +,-を< > に変更

--- src/compontents/Item.js
+++ src/compontents/Item.js
@@ -7,9 +7,9 @@ const Item = (props) => {
   return (
     <div className='item-component-item'>
       <p>{itemName}</p>
-      <button onClick={() => decreaseQuantity(index)}>-</button>
+      <button onClick={() => decreaseQuantity(index)}>{"<"}</button>
       <p>{quantity}</p>
-      <button onClick={() => increaseQuantity(index)}>+</button>
+      <button onClick={() => increaseQuantity(index)}>{">"}</button>
       <button onClick={() => deleteShopItem(index)}>削除</button>
     </div>
   )
